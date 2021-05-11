e3a127fd4822ba39cf823d25f7d5699ead82c750
Melania Antillon <mantillo@uci.edu>
2021-05-10 23:30:45 -0400

    Committing lecture changes - setting up the static file server for Heroku

---
+++ server.js
@@ -0,0 +1,30 @@
+/*********************************/
+/* Set up the static file server */
+let static = require('node-static');
+/*Set up the http server library */
+let http = require('http');
+
+/*Assume that we are running on Heroku*/
+let port = process.env.PORT;
+let directory = __dirname + '/public';
+
+/**/
+if ((typeof port == 'undefined')|| (port ===null)){
+  port = 8080;
+  directory = './public';
+}
+
+/**/
+let file = new static.Server(directory);
+
+let app = http.createServer(
+  function(request,response) {
+    request.addListener('end',
+  function() {
+    filer.serve(request,response);
+  }
+).resume();
+  }
+).listen(port);
+
+console.log('The server is running');
