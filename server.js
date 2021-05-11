111bb49ef52f2c2b36382a89898946c1cb11cb32
Melania Antillon <mantillo@uci.edu>
2021-05-11 00:02:59 -0400

    Removed random r

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ let app = http.createServer(
   function(request,response) {
     request.addListener('end',
   function() {
-    filer.serve(request,response);
+    file.serve(request,response);
   }
 ).resume();
   }
